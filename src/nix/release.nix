e6a0c5e4dfd2883aa733ff410e8fcf640ea5e410
Luke Gorrie <luke@snabb.co>
2016-03-16 09:49:57 +0000

    fixup

--- src/nix/release.nix
+++ src/nix/release.nix
@@ -2,15 +2,5 @@
 
 {
   doc = import ./manual.nix;
-  manual =
-    { doc }:
-    nixpkgs.stdenv.mkDerivation {
-      name = "manual";
-      builder = ''
-        mkdir -p $out/nix-support
-        echo "doc-pdf manual $out/share/doc/snabbswitch.pdf" \
-          >> $out/nix-support/hydra-build-products;
-      '';
-    };
 }
 
