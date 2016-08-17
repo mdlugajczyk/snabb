d870a92f39d48437f09bf8583b89c93e66b33d04
Luke Gorrie <luke@snabb.co>
2016-08-17 14:23:31 +0000

    struct packet variation B

--- src/core/packet.h
+++ src/core/packet.h
@@ -5,7 +5,8 @@ enum { PACKET_PAYLOAD_SIZE = 10*1024 };
 
 // Packet of network data, with associated metadata.
 struct packet {
-    unsigned char data[PACKET_PAYLOAD_SIZE];
     uint16_t length;           // data payload length
+    unsigned char pad[62];
+    unsigned char data[PACKET_PAYLOAD_SIZE];
 };
 
