8f18020b36dec4322475900a9584d56430539ff3
Max Rottenkolber <max@mr.gy>
2016-11-24 19:31:52 +0100

    snabbnfv: delegate port configuration to workers.

--- src/program/snabbnfv/nfvconfig.lua
+++ src/program/snabbnfv/nfvconfig.lua
@@ -13,143 +13,153 @@ local ffi = require("ffi")
 local C = ffi.C
 local lib = require("core.lib")
 
+-- Load master configuration for <pciaddr> from <ports>
+function load_control (ports, pciaddr)
+   local c = config.new()
+   local driver = assert(pci.device_info(pciaddr).driver, "Unsupported device: "..pciaddr)
+   if driver == 'apps.mellanox.connectx4' then
+      -- XXX - Configure ConnectX4 controller app here
+   end
+   return c
+end
+
+-- Return worker name for <core> argument.
+function core_worker (core)
+   if type(core) == 'number' then return core
+   elseif core               then return 'node-bound'
+   else                           return 'node-unbound' end
+end
+
+-- Compute worker set for <ports>.
+function load_workers (ports)
+   local workers = {}
+   for _,t in ipairs(ports) do workers[core_worker(t.core)] = true end
+   return workers
+end
+
 -- Return name of port in <port_config>.
 function port_name (port_config)
    return port_config.port_id:gsub("-", "_")
 end
 
--- Load NFV configuration <file> and return controller configuration for
--- <pciaddr> and ports for cores
-function load_ports (file, pciaddr)
-   local ports = lib.load_conf(file)
+-- Load app network for <ports> on <worker> with <pciaddr>, and VhostUser
+-- <socket>.
+function load_ports (ports, worker, pciaddr, sockpath)
    local c = config.new()
-   -- Configure NIC queues if applicable
-   local driver = assert(pci.device_info(pciaddr).driver, "Unsupported device: "..pciaddr)
-   if driver == 'apps.mellanox.connectx4' then
-      -- XXX - Configure ConnectX4 controller app here
+   for _,t in ipairs(ports) do
+      if core_worker(t.core) == worker then
+         load_port(c, t, pciaddr, sockpath, #ports)
+      end
    end
-   -- Organize ports by worker
-   local workers = {}
-   for _, port in ipairs(ports) do
-      local worker = port.core or false
-      workers[worker] = workers[worker] or {}
-      table.insert(workers[worker], port)
-   end
-   return c, workers
+   return c
 end
 
--- Compile app network for <ports>, <pciaddr>, and VhostUser <socket>.
-function ports_config (ports, pciaddr, sockpath)
-   local c = config.new()
-   for i,t in ipairs(ports) do
-      -- Backwards compatibity / deprecated fields
-      for deprecated, current in pairs({tx_police_gbps = "tx_police",
-                                        rx_police_gbps = "rx_police"}) do
-         if t[deprecated] and not t[current] then
-            print("Warning: "..deprecated.." is deprecated, use "..current.." instead.")
-            t[current] = t[deprecated]
-         end
+-- Configure port <t> in configuration <c> for <pciaddr> and <sockpath>.
+function load_port (c, t, pciaddr, sockpath, nports)
+   -- Backwards compatibity / deprecated fields
+   for deprecated, current in pairs({tx_police_gbps = "tx_police",
+                                     rx_police_gbps = "rx_police"}) do
+      if t[deprecated] and not t[current] then
+         print("Warning: "..deprecated.." is deprecated, use "..current.." instead.")
+         t[current] = t[deprecated]
       end
-      -- Backwards compatability end
-      local name = port_name(t)
-      local Virtio = name.."_Virtio"
-      config.app(c, Virtio, VhostUser,
-                 {socket_path=sockpath:format(t.port_id),
-                  disable_mrg_rxbuf=t.disable_mrg_rxbuf,
-                  disable_indirect_desc=t.disable_indirect_desc})
-      local VM_rx, VM_tx = Virtio..".rx", Virtio..".tx"
-      if t.tx_police then
-         local TxLimit = name.."_TxLimit"
-         local rate = t.tx_police * 1e9 / 8
-         config.app(c, TxLimit, RateLimiter, {rate = rate, bucket_capacity = rate})
-         config.link(c, VM_tx.." -> "..TxLimit..".input")
-         VM_tx = TxLimit..".output"
-      end
-      -- If enabled, track allowed connections statefully on a per-port basis.
-      -- (The table tracking connection state is named after the port ID.)
-      local pf_state_table = t.stateful_filter and name
-      if t.ingress_filter then
-         local Filter = name.."_Filter_in"
-         config.app(c, Filter, PcapFilter, { filter = t.ingress_filter,
-                                             state_table = pf_state_table })
-         config.link(c, Filter..".tx -> " .. VM_rx)
-         VM_rx = Filter..".rx"
-      end
-      if t.egress_filter then
-         local Filter = name..'_Filter_out'
-         config.app(c, Filter, PcapFilter, { filter = t.egress_filter,
-                                             state_table = pf_state_table })
-         config.link(c, VM_tx..' -> '..Filter..'.rx')
-         VM_tx = Filter..'.tx'
-      end
-      if t.tunnel and t.tunnel.type == "L2TPv3" then
-         local Tunnel = name.."_Tunnel"
-         local conf = {local_address = t.tunnel.local_ip,
-                       remote_address = t.tunnel.remote_ip,
-                       local_cookie = t.tunnel.local_cookie,
-                       remote_cookie = t.tunnel.remote_cookie,
-                       local_session = t.tunnel.session}
-         config.app(c, Tunnel, L2TPv3, conf)
-         -- Setup IPv6 neighbor discovery/solicitation responder.
-         -- This will talk to our local gateway.
-         local ND = name.."_ND"
-         config.app(c, ND, nd_light,
-                    {local_mac = t.mac_address,
-                     local_ip = t.tunnel.local_ip,
-                     next_hop = t.tunnel.next_hop})
-         -- VM -> Tunnel -> ND <-> Network
-         config.link(c, VM_tx.." -> "..Tunnel..".decapsulated")
-         config.link(c, Tunnel..".encapsulated -> "..ND..".north")
-         -- Network <-> ND -> Tunnel -> VM
-         config.link(c, ND..".north -> "..Tunnel..".encapsulated")
-         config.link(c, Tunnel..".decapsulated -> "..VM_rx)
-         VM_rx, VM_tx = ND..".south", ND..".south"
-      end
-      if t.crypto and t.crypto.type == "esp-aes-128-gcm" then
-         local Crypto = name.."_Crypto"
-         config.app(c, Crypto, AES128gcm,
-                    {spi = t.crypto.spi,
-                     transmit_key = t.crypto.transmit_key,
-                     transmit_salt = t.crypto.transmit_salt,
-                     receive_key = t.crypto.receive_key,
-                     receive_salt = t.crypto.receive_salt,
-                     auditing = t.crypto.auditing})
-         config.link(c, VM_tx.." -> "..Crypto..".decapsulated")
-         config.link(c, Crypto..".decapsulated -> "..VM_rx)
-         VM_rx, VM_tx = Crypto..".encapsulated", Crypto..".encapsulated"
-      end
-      if t.rx_police then
-         local RxLimit = name.."_RxLimit"
-         local rate = t.rx_police * 1e9 / 8
-         config.app(c, RxLimit, RateLimiter, {rate = rate, bucket_capacity = rate})
-         config.link(c, RxLimit..".output -> "..VM_rx)
-         VM_rx = RxLimit..".input"
-      end
-      local NIC = name.."_NIC"
-      local driver = assert(pci.device_info(pciaddr).driver, "Unsupported device: "..pciaddr)
-      if driver == 'apps.mellanox.connectx4' then
-         -- XXX - Configure ConnectX4 queue app here
-      elseif (driver == 'apps.intel.intel_app' or
-              driver == 'apps.solarflare.solarflare') then
-         local vmdq = true
-         if not t.mac_address then
-            assert(#ports == 1, "multiple ports defined but promiscuous mode requested for port: "..name)
-            vmdq = false
-         end
-         config.app(c, NIC, require(driver).driver,
-                    {pciaddr = pciaddr,
-                     vmdq = vmdq,
-                     macaddr = t.mac_address,
-                     vlan = t.vlan})
-      else
-         error("Unsupported driver: "..driver)
-      end
-      config.link(c, NIC..".tx".." -> "..VM_rx)
-      config.link(c, VM_tx.." -> "..NIC..".rx")
    end
-
-   -- Return configuration c.
-   return c
+   -- Backwards compatability end
+   local name = port_name(t)
+   local Virtio = name.."_Virtio"
+   config.app(c, Virtio, VhostUser,
+              {socket_path=sockpath:format(t.port_id),
+               disable_mrg_rxbuf=t.disable_mrg_rxbuf,
+               disable_indirect_desc=t.disable_indirect_desc})
+   local VM_rx, VM_tx = Virtio..".rx", Virtio..".tx"
+   if t.tx_police then
+      local TxLimit = name.."_TxLimit"
+      local rate = t.tx_police * 1e9 / 8
+      config.app(c, TxLimit, RateLimiter, {rate = rate, bucket_capacity = rate})
+      config.link(c, VM_tx.." -> "..TxLimit..".input")
+      VM_tx = TxLimit..".output"
+   end
+   -- If enabled, track allowed connections statefully on a per-port basis.
+   -- (The table tracking connection state is named after the port ID.)
+   local pf_state_table = t.stateful_filter and name
+   if t.ingress_filter then
+      local Filter = name.."_Filter_in"
+      config.app(c, Filter, PcapFilter, { filter = t.ingress_filter,
+                                          state_table = pf_state_table })
+      config.link(c, Filter..".tx -> " .. VM_rx)
+      VM_rx = Filter..".rx"
+   end
+   if t.egress_filter then
+      local Filter = name..'_Filter_out'
+      config.app(c, Filter, PcapFilter, { filter = t.egress_filter,
+                                          state_table = pf_state_table })
+      config.link(c, VM_tx..' -> '..Filter..'.rx')
+      VM_tx = Filter..'.tx'
+   end
+   if t.tunnel and t.tunnel.type == "L2TPv3" then
+      local Tunnel = name.."_Tunnel"
+      local conf = {local_address = t.tunnel.local_ip,
+                    remote_address = t.tunnel.remote_ip,
+                    local_cookie = t.tunnel.local_cookie,
+                    remote_cookie = t.tunnel.remote_cookie,
+                    local_session = t.tunnel.session}
+      config.app(c, Tunnel, L2TPv3, conf)
+      -- Setup IPv6 neighbor discovery/solicitation responder.
+      -- This will talk to our local gateway.
+      local ND = name.."_ND"
+      config.app(c, ND, nd_light,
+                 {local_mac = t.mac_address,
+                  local_ip = t.tunnel.local_ip,
+                  next_hop = t.tunnel.next_hop})
+      -- VM -> Tunnel -> ND <-> Network
+      config.link(c, VM_tx.." -> "..Tunnel..".decapsulated")
+      config.link(c, Tunnel..".encapsulated -> "..ND..".north")
+      -- Network <-> ND -> Tunnel -> VM
+      config.link(c, ND..".north -> "..Tunnel..".encapsulated")
+      config.link(c, Tunnel..".decapsulated -> "..VM_rx)
+      VM_rx, VM_tx = ND..".south", ND..".south"
+   end
+   if t.crypto and t.crypto.type == "esp-aes-128-gcm" then
+      local Crypto = name.."_Crypto"
+      config.app(c, Crypto, AES128gcm,
+                 {spi = t.crypto.spi,
+                  transmit_key = t.crypto.transmit_key,
+                  transmit_salt = t.crypto.transmit_salt,
+                  receive_key = t.crypto.receive_key,
+                  receive_salt = t.crypto.receive_salt,
+                  auditing = t.crypto.auditing})
+      config.link(c, VM_tx.." -> "..Crypto..".decapsulated")
+      config.link(c, Crypto..".decapsulated -> "..VM_rx)
+      VM_rx, VM_tx = Crypto..".encapsulated", Crypto..".encapsulated"
+   end
+   if t.rx_police then
+      local RxLimit = name.."_RxLimit"
+      local rate = t.rx_police * 1e9 / 8
+      config.app(c, RxLimit, RateLimiter, {rate = rate, bucket_capacity = rate})
+      config.link(c, RxLimit..".output -> "..VM_rx)
+      VM_rx = RxLimit..".input"
+   end
+   local NIC = name.."_NIC"
+   local driver = assert(pci.device_info(pciaddr).driver, "Unsupported device: "..pciaddr)
+   if driver == 'apps.mellanox.connectx4' then
+      -- XXX - Configure ConnectX4 queue app here
+   elseif (driver == 'apps.intel.intel_app' or
+           driver == 'apps.solarflare.solarflare') then
+      local vmdq = true
+      if not t.mac_address then
+         assert(nports == 1, "multiple ports defined but promiscuous mode requested for port: "..name)
+         vmdq = false
+      end
+      config.app(c, NIC, require(driver).driver,
+                 {pciaddr = pciaddr,
+                  vmdq = vmdq,
+                  macaddr = t.mac_address,
+                  vlan = t.vlan})
+   else
+      error("Unsupported driver: "..driver)
+   end
+   config.link(c, NIC..".tx".." -> "..VM_rx)
+   config.link(c, VM_tx.." -> "..NIC..".rx")
 end
 
 function selftest ()
@@ -170,15 +180,16 @@ function selftest ()
                               "program/snabbnfv/test_fixtures/nfvconfig/scale_change/y"})
    do
       print("testing:", confpath)
-      local m, workers = load_ports(confpath, pcideva)
-      local w = ports_config(workers[false], pcideva, "/dev/null")
-      engine.configure(m)
-      engine.main({duration = 0.25})
-      engine.configure(w)
+      local ports = lib.load_conf(confpath)
+      local c = load_control(ports, pcideva)
+      for _, port in ipairs(ports) do
+         load_port(c, port, pcideva, "/dev/null")
+      end
+      engine.configure(c)
       engine.main({duration = 0.25})
    end
-   local m, workers = load_ports("program/snabbnfv/test_fixtures/nfvconfig/test_functions/deprecated.port", pcideva, "/dev/null")
-   local w = ports_config(workers[false], pcideva, "/dev/null")
-   assert(w.apps["Test_TxLimit"])
-   assert(w.apps["Test_RxLimit"])
+   local c = load_ports(lib.load_conf("program/snabbnfv/test_fixtures/nfvconfig/test_functions/deprecated.port"),
+                        'node-unbound', pcideva, "/dev/null")
+   assert(c.apps["Test_TxLimit"])
+   assert(c.apps["Test_RxLimit"])
 end
--- src/program/snabbnfv/traffic/traffic.lua
+++ src/program/snabbnfv/traffic/traffic.lua
@@ -4,6 +4,7 @@ module(..., package.seeall)
 
 local lib = require("core.lib")
 local worker = require("core.worker")
+local counter = require("core.counter")
 local nfvconfig = require("program.snabbnfv.nfvconfig")
 local usage = require("program.snabbnfv.traffic.README_inc")
 local ffi = require("ffi")
@@ -11,7 +12,7 @@ local C = ffi.C
 local timer = require("core.timer")
 local pci = require("lib.hardware.pci")
 local ingress_drop_monitor = require("lib.timers.ingress_drop_monitor")
-local counter = require("core.counter")
+local numa = require("lib.numa")
 
 local long_opts = {
    benchmark     = "B",
@@ -25,81 +26,79 @@ local long_opts = {
 
 function run (args)
    local opt = {}
-   local worker_args = {
+   local param = {
       linkreportinterval = 0,
       loadreportinterval = 1,
       debugreportinterval = 0
    }
-   function opt.B (arg) worker_args.benchpackets = tonumber(arg) end
+   function opt.B (arg) param.benchpackets = tonumber(arg) end
    function opt.h (arg) print(short_usage()) main.exit(1) end
    function opt.H (arg) print(long_usage())  main.exit(1) end
-   function opt.k (arg) worker_args.linkreportinterval = tonumber(arg) end
-   function opt.l (arg) worker_args.loadreportinterval = tonumber(arg) end
-   function opt.D (arg) worker_args.debugreportinterval = tonumber(arg) end
-   function opt.b (arg) worker_args.busywait = true end
+   function opt.k (arg) param.linkreportinterval = tonumber(arg) end
+   function opt.l (arg) param.loadreportinterval = tonumber(arg) end
+   function opt.D (arg) param.debugreportinterval = tonumber(arg) end
+   function opt.b (arg) param.busywait = true end
    args = lib.dogetopt(args, opt, "hHB:k:l:D:b", long_opts)
    if #args == 3 then
-      local pciaddr, confpath, sockpath = unpack(args)
-      local ok, info = pcall(pci.device_info, pciaddr)
+      param.pciaddr, param.confpath, param.sockpath = unpack(args)
+      local ok, info = pcall(pci.device_info, param.pciaddr)
       if not ok then
-         print("Error: device not found " .. pciaddr)
+         print("Error: device not found " .. param.pciaddr)
          os.exit(1)
       end
       if not info.driver then
-         print("Error: no driver for device " .. pciaddr)
+         print("Error: no driver for device " .. param.pciaddr)
          os.exit(1)
       end
-      worker_args.pciaddr = pciaddr
-      worker_args.sockpath = sockpath
-      if worker_args.benchpackets then
-         print("Loading " .. confpath)
-         local c, workers = nfvconfig.load_ports(confpath, pciaddr)
+      if param.benchpackets then
+         print("Loading " .. param.confpath)
+         local ports = lib.load_conf(param.confpath)
+         local c = nfvconfig.load_control(ports, param.pciaddr)
+         local workers = nfvconfig.load_workers(ports)
          engine.configure(c)
-         for core, ports in pairs(workers) do
+         for w, _ in pairs(workers) do
             worker.start(
-               worker_name(core),
+               worker_name(w),
                'require("program.snabbnfv.traffic.traffic").nfv_worker('..
-                  lib.conf_string(worker_args)..','..
-                  lib.conf_string(ports)..','..
-                  tostring(core)..')'
+                  lib.conf_string(param)..','..
+                  lib.conf_string(w)..')'
             )
          end
          local function workers_done ()
             for _, status in pairs(worker.status()) do
                if status.alive then return false end
             end
+            return true
          end
          engine.main({done = workers_done})
       else
-         local mtime = 0
-         local needs_reconfigure = true
-         function check_for_reconfigure()
-            needs_reconfigure = C.stat_mtime(confpath) ~= mtime
+         local workers = {}
+         local function loader (ports)
+            engine.configure(nfvconfig.load_control(ports, param.pciaddr))
+            local new_workers = nfvconfig.load_workers(ports)
+            -- Stop obsolete workers
+            for w in pairs(workers) do
+               if not new_workers[w] then
+                  print("Stopping: "..worker_name(w))
+                  worker.stop(worker_name(w))
+               end
+            end
+            -- Start new workers
+            for w in pairs(new_workers) do
+               if not workers[w] then
+                  print("Starting: "..worker_name(w))
+                  worker.start(
+                     worker_name(w),
+                     'require("program.snabbnfv.traffic.traffic").nfv_worker('..
+                        lib.conf_string(param)..','..
+                        lib.conf_string(w)..')'
+                  )
+               end
+            end
+            -- Remember active worker set
+            workers = new_workers
          end
-         timer.activate(timer.new("reconf", check_for_reconfigure, 1e9, 'repeating'))
-         while true do
-            needs_reconfigure = false
-            print("Loading " .. confpath)
-            mtime = C.stat_mtime(confpath)
-            if mtime == 0 then
-               print(("WARNING: File '%s' does not exist."):format(confpath))
-            end
-            local c, workers = nfvconfig.load_ports(confpath, pciaddr)
-            engine.configure(c)
-            for core, ports in pairs(workers) do
-               worker.start(
-                  worker_name(core),
-                  'require("program.snabbnfv.traffic.traffic").nfv_worker('..
-                     lib.conf_string(worker_args)..','..
-                     lib.conf_string(ports)..','..
-                     tostring(core)..')'
-               )
-            end
-            engine.main({done=function() return needs_reconfigure end})
-            for name, status in pairs(worker.status()) do
-               if status.alive then worker.stop(name) end
-            end
-         end
+         with_nfvconf(param.confpath, loader, 'verbose')
       end
    else
       print("Wrong number of arguments: " .. tonumber(#args))
@@ -112,58 +111,51 @@ end
 function short_usage () return (usage:gsub("%s*CONFIG FILE FORMAT:.*", "")) end
 function long_usage () return usage end
 
-function worker_name (core)
-   if type(core) == 'number' then return "worker("..core..")"
-   elseif  core              then return "worker(bound)"
-   else                           return "worker(unbound)"    end
-end
-
-function nfv_worker (args, ports, core)
-   if args.busywait then engine.busywait = true end
-   if type(core) == 'number' then
-      numa.bind_to_cpu(core)
-   elseif core then
-      numa.bind_to_numa_node(numa.pci_get_numa_node(args.pciaddr))
+function nfv_worker (param, worker)
+   if param.busywait then engine.busywait = true end
+   if type(worker) == 'number' then
+      numa.bind_to_cpu(worker)
+   elseif worker == 'node-bound' then
+      numa.bind_to_numa_node(numa.pci_get_numa_node(param.pciaddr))
    end
-   local voice = worker_name(core)..": "
-   local function say (report_fn)
-      return function () io.write(voice); report_fn() end
-   end
-   if args.loadreportinterval > 0 then
-      local t = timer.new("nfvloadreport", say(engine.report_load), args.loadreportinterval*1e9, 'repeating')
+   if param.loadreportinterval > 0 then
+      local t = timer.new("nfvloadreport", say(worker, engine.report_load), param.loadreportinterval*1e9, 'repeating')
       timer.activate(t)
    end
-   if args.linkreportinterval > 0 then
-      local t = timer.new("nfvlinkreport", say(engine.report_links), args.linkreportinterval*1e9, 'repeating')
+   if param.linkreportinterval > 0 then
+      local t = timer.new("nfvlinkreport", say(worker, engine.report_links), param.linkreportinterval*1e9, 'repeating')
       timer.activate(t)
       end
-   if args.debugreportinterval > 0 then
-      local t = timer.new("nfvdebugreport", say(engine.report_apps), args.debugreportinterval*1e9, 'repeating')
+   if param.debugreportinterval > 0 then
+      local t = timer.new("nfvdebugreport", say(worker, engine.report_apps), param.debugreportinterval*1e9, 'repeating')
       timer.activate(t)
    end
-   if args.benchpackets then
-      print(voice.."starting (benchmark mode)")
-      bench(args.pciaddr, ports, args.sockpath, args.benchpackets)
+   if param.benchpackets then
+      say(worker, function () print("starting (benchmark mode)") end)()
+      bench(param.pciaddr, param.confpath, param.sockpath, worker, param.benchpackets)
    else
-      print(voice.."starting")
-      traffic(args.pciaddr, ports, args.sockpath)
+      say(worker, function () print("starting") end)()
+      traffic(param.pciaddr, param.confpath, param.sockpath, worker)
    end
 end
 
 -- Run in real traffic mode.
-function traffic (pciaddr, ports, sockpath)
+function traffic (pciaddr, confpath, sockpath, worker)
    -- Flush logs every second.
    timer.activate(timer.new("flush", io.flush, 1e9, 'repeating'))
    timer.activate(ingress_drop_monitor.new({action='warn'}):timer())
-   engine.configure(nfvconfig.ports_config(ports, pciaddr, sockpath))
-   engine.main()
+   local function loader (ports)
+      engine.configure(nfvconfig.load_ports(ports, worker, pciaddr, sockpath))
+   end
+   with_nfvconf(confpath, loader)
 end
 
 -- Run in benchmark mode.
-function bench (pciaddr, ports, sockpath, npackets)
+function bench (pciaddr, confpath, sockpath, worker, npackets)
+   local ports = lib.load_conf(confpath)
    local nic = (nfvconfig.port_name(ports[1])).."_NIC"
 
-   engine.configure(nfvconfig.ports_config(ports, pciaddr, sockpath))
+   engine.configure(nfvconfig.load_ports(ports, worker, pciaddr, sockpath))
 
    -- From designs/nfv
    local start, packets, bytes = 0, 0, 0
@@ -205,3 +197,30 @@ function bench (pciaddr, ports, sockpath, npackets)
    require("jit.p").stop()
 end
 
+function worker_name (worker)
+   return "worker("..worker..")"
+end
+
+function say (worker, report_fn)
+   local voice = worker_name(worker)..": "
+   return function () io.write(voice); report_fn() end
+end
+
+function with_nfvconf (confpath, loader, verbose)
+   local mtime = 0
+   local needs_reconfigure = true
+   function check_for_reconfigure()
+      needs_reconfigure = C.stat_mtime(confpath) ~= mtime
+   end
+   timer.activate(timer.new("reconf", check_for_reconfigure, 1e9, 'repeating'))
+   while true do
+      needs_reconfigure = false
+      if verbose then print("Loading " .. confpath) end
+      mtime = C.stat_mtime(confpath)
+      if mtime == 0 and verbose then
+         print(("WARNING: File '%s' does not exist."):format(confpath))
+      end
+      loader(lib.load_conf(confpath))
+      engine.main({done=function() return needs_reconfigure end})
+   end
+end
